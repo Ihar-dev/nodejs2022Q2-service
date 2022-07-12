36eb484caad188984aac39157f77d713cf12120e
Ihar Kozik <ihar.kozik@gmail.com>
2022-07-13 00:38:00 +0300

    feat: edit swagger methods

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,30 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule } from '@nestjs/swagger';
-import { dirname, join } from 'path';
-import { parse } from 'yaml';
-import { readFile } from 'fs/promises';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+//import { dirname, join } from 'path';
+//import { parse } from 'yaml';
+//import { readFile } from 'fs/promises';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const rootDirname = dirname(__dirname);
+  const port = process.env.PORT || 4000;
+
+  const config = new DocumentBuilder()
+    .setTitle('REST Service')
+    .setDescription('Music Home Library')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  /* const rootDirname = dirname(__dirname);
   const DOC_API = await readFile(
     join(rootDirname, './doc', 'api.yaml'),
     'utf-8',
   );
-  const document = parse(DOC_API);
+  const document = parse(DOC_API); */
 
   SwaggerModule.setup('doc', app, document);
-  await app.listen(8000);
+  await app.listen(port);
 }
 bootstrap();
