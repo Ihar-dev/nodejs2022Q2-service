1f3c53e7516b4a2eb7a4da714440d59670134797
Ihar Kozik <ihar.kozik@gmail.com>
2022-07-17 01:21:24 +0300

    feat: add tracks module

---
+++ src/tracks/dto/create-track.dto.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateTrackDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty({ example: '92e0a93a-59ce-4220-8ef6-def797c2440e' })
+  @IsNotEmpty()
+  @IsString()
+  artistId: string | null;
+
+  @ApiProperty({ example: '441f4f18-d265-4af2-8d54-2cb5d1e15b9b' })
+  @IsNotEmpty()
+  @IsString()
+  albumId: string | null;
+
+  @ApiProperty({ example: 123 })
+  @IsNotEmpty()
+  @IsInt()
+  duration: number;
+}
---
+++ src/tracks/tracks.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { TracksService } from './tracks.service';
+import { CreateTrackDto } from './dto/create-track.dto';
+import { UpdateTrackDto } from './dto/update-track.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Tracks')
+@Controller('track')
+export class TracksController {
+  constructor(private readonly tracksService: TracksService) {}
+
+  @Post()
+  @ApiOperation({
+    summary: 'create new track',
+    description: 'Creates a new track.',
+  })
+  create(@Body() createTrackDto: CreateTrackDto) {
+    return this.tracksService.create(createTrackDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.tracksService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.tracksService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTrackDto: UpdateTrackDto) {
+    return this.tracksService.update(+id, updateTrackDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.tracksService.remove(+id);
+  }
+}
