d4ca1696b605fdb6dc8ba11ead4fb4f1ac12c519
Ihar Kozik <ihar.kozik@gmail.com>
2022-07-30 19:44:43 +0300

    refactor: remove unused methods from ArtistsService

--- src/artists/artists.service.ts
+++ src/artists/artists.service.ts
@@ -1,28 +1,13 @@
-import {
-  forwardRef,
-  Inject,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { CreateUpdateArtistDto } from './dto/create-update-artist.dto';
 import { Artist } from './entities/artist.entity';
-import { AlbumsService } from '../albums/albums.service';
-import { TracksService } from '../tracks/tracks.service';
-import { FavoritesService } from '../favorites/favorites.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { plainToInstance } from 'class-transformer';
 
 @Injectable()
 export class ArtistsService {
-  constructor(
-    private readonly albumsService: AlbumsService,
-    @Inject(forwardRef(() => TracksService))
-    private readonly tracksService: TracksService,
-    @Inject(forwardRef(() => FavoritesService))
-    private readonly favoritesService: FavoritesService,
-    private readonly prisma: PrismaService,
-  ) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   public async create(
     createUpdateArtistDto: CreateUpdateArtistDto,
@@ -73,9 +58,6 @@ export class ArtistsService {
         where: { id },
       });
 
-      try {
-        // await this.favoritesService.removeArtist(id);
-      } catch (err) {}
       return 'The artist has been deleted';
     } else throw new NotFoundException();
   }
