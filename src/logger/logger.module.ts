b5711211d3c78459e43cadbb8a331d1f89c3705e
Ihar Kozik <ihar.kozik@gmail.com>
2022-08-07 16:03:45 +0300

    feat: add LoggingService

--- src/logger/logger.module.ts
+++ src/logger/logger.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { LoggingService } from './logging.service';
 
-@Module({})
+@Module({
+  providers: [LoggingService],
+})
 export class LoggerModule {}
