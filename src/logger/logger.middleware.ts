3327d41eef270656f26d6415f92f6830701f6ae3
Ihar Kozik <ihar.kozik@gmail.com>
2022-08-07 17:19:05 +0300

    feat: add LoggingService

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -1,5 +1,6 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction } from 'express';
+import { inspect } from 'util';
 import { LoggingService } from './logging.service';
 
 @Injectable()
@@ -7,7 +8,9 @@ export class LoggerMiddleware implements NestMiddleware {
   constructor(private logger: LoggingService) {}
 
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('Request...');
+    const { body } = req;
+    const logString = `body: ${inspect(body)}`;
+    this.logger.log(logString);
     next();
   }
 }
--- src/logger/logging.service.ts
+++ src/logger/logging.service.ts
@@ -2,13 +2,7 @@ import { ConsoleLogger, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class LoggingService extends ConsoleLogger {
-  log(message: unknown, context?: unknown): void {
-    super.log(message, context);
-  }
-
-  error(message: any, stack?: string, context?: string): void;
-  error(message: any, ...optionalParams: any[]): void;
-  error(message: unknown, context?: unknown): void {
-    super.error(message, context);
+  log(message: string): void {
+    super.log(message);
   }
 }
